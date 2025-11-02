e2bd4fc89c1cfdf7fffc8ee4be54d4e9e8daf587
fagnercmalheiros <fagnercmalheiros@gmail.com>
2025-11-02 14:20:40 -0300

    adicionando icone da despesa

--- scripts.js
+++ scripts.js
@@ -3,6 +3,7 @@ const form = document.querySelector("form")
 const amount = document.getElementById("amount")
 const expense = document.getElementById("expense")
 const category = document.getElementById("category")
+const expenseList = document.querySelector("ul")
 
 //CAPTURA DO EVENTO E FORMATAR O VALOR
 
@@ -26,4 +27,46 @@ function formatCurrencyBRL(value){
         currency: "BRL"
     })
     return value
+}
+
+//captura evento de subimit do form
+form,onsubmit = (event) => {
+//previne comport. de att automatica
+event.preventDefault()
+
+//cria objeto detalhado da nova despesa
+
+const newExpense = {
+    id: new Date().getTime(),
+    expense: expense.value,
+    category_id: category.value,
+    category_name: category.options[category.selectedIndex].text,
+    amount: amount.value,
+    created_at: new Date()
+}
+
+expenseAdd(newExpense)
+}
+
+// adição dos itens na lista
+
+function expenseAdd(newExpense) {
+// cria elemento para add na lista
+const expenseItem = document.createElement("li")
+expenseItem.classList.add("expense")
+
+//crindo o icone da categoria
+
+const expenseIcon = document.createElement("img")
+expenseIcon.setAttribute("src",`img/${newExpense.category_id}.svg`)
+expenseIcon.setAttribute("alt", newExpense.category_name)
+
+//add info do item
+expenseItem.append(expenseIcon)
+
+//add itens na lista
+expenseList.append(expenseItem)
+
+
+
 }
